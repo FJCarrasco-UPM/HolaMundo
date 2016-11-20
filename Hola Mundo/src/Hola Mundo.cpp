096cd773e4a1b202270d334e91dbc8c1f1e49c31
Fran <franciscojose.carrasco.martinez@alumnos.upm.es>
2016-11-20 09:25:17 +0100

    Creado Proyecto Hola Mundo 2

---
+++ Hola Mundo/src/Hola Mundo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hola.cpp
+// Author      : Francisco José Carrasco
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello it´s me - XXXX" << endl; //
+	return 0;
+}
